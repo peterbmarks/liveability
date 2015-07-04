6ac625fb1306e32649513d0d03e59fdbe6e514eb
Peter Marks <peter.marks@pobox.com>
2015-07-05 07:42:56 +1000

    Tried to set the tint colour.

--- Liveability/AppDelegate.m
+++ Liveability/AppDelegate.m
@@ -8,6 +8,11 @@
 
 #import "AppDelegate.h"
 #import "ShowMapViewController.h"
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+alpha:1.0]
 
 @interface AppDelegate () <UISplitViewControllerDelegate>
 
@@ -24,6 +29,7 @@
     splitViewController.delegate = self;
     
     self.dataManager = [DataManager new];
+    [[UIButton appearance] setTintColor:UIColorFromRGB(0x36b1bf)]; 
     return YES;
 }
 
